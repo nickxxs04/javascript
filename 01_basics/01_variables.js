5ea1f2ba5bbfa2ad44485e9262bffe181f914f9e
nickxxs04 <nickxxs04@gmail.com>
2026-04-13 11:03:05 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 220099
+let accountEmail ="njjkk@gmail.com"
+var accountPassword ="12345"
+accountCity ="noida"
+//accountState;
+
+//accountId = 33
+accountEmail="hdd@gmail.com"
+accountPassword="34532"
+accountCity="varanashi"
+
+
+console.log(accountId);
+console.log([accountId,accountEmail,accountPassword,accountCity]);
