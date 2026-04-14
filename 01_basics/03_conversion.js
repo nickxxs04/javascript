bd048a29180388708151d24e79e860b928451c44
nickxxs04 <nickxxs04@gmail.com>
2026-04-14 08:53:13 +0000

    conversion and operation

--- 01_basics/03_conversion.js
+++ 01_basics/03_conversion.js
@@ -10,10 +10,49 @@ let valueInNumber = Number(
 
 let isLoggedIn ="nikhil"// for 1 -true, for " " -false,for "nikhil"-true
 let booleanIsLoggedIn =Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+//console.log(booleanIsLoggedIn);
 
 let someNumber = 04
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+//console.log(stringNumber);
+//console.log(typeof stringNumber);
+
+
+// *********************operations*************************//
+
+let value = 3
+let negValue = -value
+//console.log(negValue);
+/*
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**2);
+console.log(2/2);
+console.log(2%2);
+*/
+
+let str1 ="hello"
+let str2 =" nikhil"
+let str3 = str1 + str2
+//console.log(str3);
+
+//console.log("1"+2);// problem occurs here shows 12 as a output
+//console.log(1+"2");// problem occurs here shows 12 as a output
+//console.log("1"+2 +2);// shows 122
+//console.log(1+ 2+ "2");// shows 32
+
+console.log(+true);//after using + its shows 1 as a output
+
+
+let num1, num2, num3
+
+num1 = num2 = num3= 2+2
+
+let gameCounter = 100
+//gameCounter++;
+++gameCounter;
+console.log(gameCounter);
+
+
 
